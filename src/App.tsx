e1a89f5d2b8dfad910110c2c53c2ac14b730fb4e
bhadas <bhadas@ford.com>
2021-11-09 10:11:54 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Button } from '@material-ui/core'
+
+function App() {
+  return (
+    <><h1>Food App</h1><Button variant="contained">Contained</Button></>
+  );
+}
+
+export default App;
+
